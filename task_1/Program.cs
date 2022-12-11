a9aa74b1de6d3587149eb23947545b743882da10
antonbashlovka <anton.bashlovka@gmail.com>
2022-12-11 14:33:28 +0500

    done task 1

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. Не использовать функцию Math.Pow
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+
+string number;
+int baseNumber;
+int expNumber;
+int i;
+
+
+Console.WriteLine("Введите число для возведения в степень");
+number = Console.ReadLine();
+while (int.TryParse(number, out baseNumber) == false){
+    Console.WriteLine("Ошибка. Вы ввели текст, необходимо число. Попробуйте еще раз");
+    number = Console.ReadLine();   
+}
+
+Console.WriteLine("Введите показатель степени");
+number = Console.ReadLine();
+while (int.TryParse(number, out expNumber) == false){
+    Console.WriteLine("Ошибка. Вы ввели текст, необходимо число. Попробуйте еще раз");
+    number = Console.ReadLine();   
+}
+
+int res = baseNumber;
+
+for (i = 1; i < expNumber; i++)
+{
+    res = res * baseNumber;
+}
+
+Console.WriteLine("результат " + res);
