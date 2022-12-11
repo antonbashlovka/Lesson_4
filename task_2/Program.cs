5c6c6029048bd11038cb86b5c1b47a7a8f2dc705
antonbashlovka <anton.bashlovka@gmail.com>
2022-12-11 15:07:44 +0500

    done task 2

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+    Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+    452 -> 11
+    82 -> 10
+    9012 -> 12
+*/
+
+int intNumber;
+
+int inputNumber (string message){
+    int res;
+    string number;
+    Console.WriteLine(message);
+    number = Console.ReadLine();
+    while (int.TryParse(number, out res) == false){
+        Console.WriteLine("Ошибка. Вы ввели текст, необходимо число. Попробуйте еще раз");
+        number = Console.ReadLine();   
+    }
+    return res;
+}
+
+intNumber = inputNumber("Введите число для расчета суммы цифр в нем");
+
+int sum = 0;
+
+while (intNumber > 0) {
+    sum = sum + intNumber % 10;
+    intNumber = intNumber / 10;
+}
+
+Console.WriteLine(sum);
